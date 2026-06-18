055f2ca0fd026f4c5dcd86942a1a87d21f8d16d9
Aryan Sarkar <aryansarkar9400@gmail.com>
2026-06-18 18:19:28 +0530

    Clean Task 1 Repo (Frontend Only)

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-        <div class="footer-container">
-            <p>&copy; <?php echo date('Y'); ?> <?php echo htmlspecialchars($my_name ?? 'Aryan Sarkar'); ?> - Apex Planet Portfolio</p>
-        </div>
-    </footer>
-</body>
-</html>
